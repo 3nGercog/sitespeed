72cb5c85158c96fc33cc93821fc57a771926c093
3nGercog <sereqa0813@rambler.ru>
2017-04-20 18:33:44 +0300

    sec

---
+++ sitespeed/sitespeed/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sitespeed.MvcApplication" Language="C#" %>
---
+++ sitespeed/sitespeed/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sitespeed.Startup))]
+namespace sitespeed
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
